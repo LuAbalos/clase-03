301c61aa5f9317606b100ff023384292bf2366aa
Lu Abalos <abaloslucia@gmail.com>
2022-03-29 14:54:36 -0300

    Create Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <ul className="navegador">    
+                <li>
+                     <h2> MARINO VARETA </h2>
+                    
+                </li>
+                <li>
+                     <span> Home </span>
+                </li>
+                
+                <li>
+                   <span> Servicios </span>
+                </li>
+                
+                <li>
+                   <span> Productos </span>
+                </li>
+                
+                <li>
+                    <span> Contacto</span>
+                </li>
+                
+                <li>
+                    <span> Quienes somos</span>
+                </li>
+                
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
